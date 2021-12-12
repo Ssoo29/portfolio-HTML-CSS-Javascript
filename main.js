4d5b0ff260374f27caa15088cfd08db20c29a9bc
Sungsoo <45908591+Ssoo29@users.noreply.github.com>
2021-12-12 10:50:31 +0900

    JS: navbar scroll

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strick';
+
+// Make navbar transparent whein it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeigth = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  console.log("1: ", window.scrollY);
+  console.log("2: ", navbarHeigth);
+  if(window.scrollY > navbarHeigth) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
